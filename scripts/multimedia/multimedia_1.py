871254429682d6cbc6db62f9216f047c2abf7fc6
VasilyevEvgeny <vasilev.evgeniy@physics.msu.ru>
2019-06-19 15:37:44 +0300

    resources

--- scripts/multimedia/multimedia_1.py
+++ scripts/multimedia/multimedia_1.py
@@ -28,8 +28,8 @@ class Multimedia1(BaseMultimedia):
                               lmbda=1800 * 10 ** -9,
                               x_0=100 * 10 ** -6,
                               y_0=100 * 10 ** -6,
-                              n_x=512,
-                              n_y=512,
+                              n_x=2048,
+                              n_y=2048,
                               radii_in_grid=20)
 
                 visualizer = BeamVisualizer(beam=beam,
@@ -45,9 +45,9 @@ class Multimedia1(BaseMultimedia):
                                         n_z=3000,
                                         dz_0=10**-5,
                                         const_dz=True,
-                                        print_current_state_every=50,
+                                        print_current_state_every=10,
                                         max_intensity_to_stop=4*10**16,
-                                        plot_beam_every=50,
+                                        plot_beam_every=10,
                                         visualizer=visualizer)
                 propagator.propagate()
 
