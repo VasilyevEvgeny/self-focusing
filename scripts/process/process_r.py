c541b60e2ebb0a9fade40e5dbff9949f70d914a9
Evgeny Vasilyev <vasilev.evgeniy@physics.msu.ru>
2021-11-01 23:16:27 +0300

    Dissertation process_r

--- scripts/process/process_r.py
+++ scripts/process/process_r.py
@@ -7,8 +7,8 @@ args = parse_args()
 # create object of 3D axisymmetric beam
 beam = BeamR(medium='SiO2',
              p_0_to_p_vortex=9,
-             m=1,
-             M=1,
+             m=4,
+             M=4,
              lmbda=1800*10**-9,
              r_0=100*10**-6,
              n_r=1024)
@@ -28,7 +28,7 @@ propagator = Propagator(args=args,
                         dz_0=beam.z_diff / 1000,
                         const_dz=False,
                         print_current_state_every=1,
-                        plot_beam_every=50,
+                        plot_beam_every=1000,
                         max_intensity_to_stop=5e17,
                         visualizer=visualizer)
 
